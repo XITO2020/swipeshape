15ad142113906e566e9e1b7a72c01353f1ca2dec
xitotabasco2020 <orland.opal1927@gmail.com>
2025-06-17 15:53:13 +0200

    fix ports , variables, docker.sh

--- deploy.sh
+++ deploy.sh
@@ -1,16 +1,13 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# Script to build and deploy SwipeShape app
-
-# Build the Docker image
 echo "Building Docker image..."
 docker build -t swipeshape:latest .
 
-# Run the container
 echo "Starting container..."
-docker run -d -p 3000:3000 \
-  --env-file .env.production \
+docker run -d \
+  -p 8080:3000 \                # expose 3000 du conteneur sur 8080 de la VM
+  --env-file .env \             # charge ton .env existant
   --name swipeshape-app \
   swipeshape:latest
 
-echo "Deployment complete! App is running at http://localhost:3000"
+echo "Done! App is running at http://localhost:8080 (ou http://91.99.110.166:8080 en prod)"
