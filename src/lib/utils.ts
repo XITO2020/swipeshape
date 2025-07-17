cd3e89c04adbb15f2ac5b294e5a46a022e75fd9a
xitotabasco2020 <orland.opal1927@gmail.com>
2025-07-17 11:40:20 +0200

    suppression utilisation de twmerge

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,12 +1,10 @@
 // src/lib/utils.ts
 import { ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
 
 /**
- * Merges class names using clsx and tailwind-merge
- * This allows for conditional class name application and proper
- * handling of Tailwind classes
+ * Merges class names using clsx
+ * This allows for conditional class name application
  */
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs));
+  return clsx(inputs);
 }
