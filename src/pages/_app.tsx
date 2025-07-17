d4d6a18411e1ebc1def54f91dbd44cc1ab01b318
xitotabasco2020 <orland.opal1927@gmail.com>
2025-07-17 13:47:01 +0200

    bootsrap etait pas la, tres bizarre tout ça

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { AppProps } from 'next/app';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import '../index.css';
 import { useAppStore } from '../lib/store';
 import Sidebar from '../components/SideBar';
